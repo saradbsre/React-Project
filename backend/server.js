72b79ea6ad76d02b60888784544c166a7e523cde
saradbsre <sarad@binshabibgroup.ae>
2025-06-12 10:13:11 +0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -365,15 +365,15 @@ LEFT JOIN Tenants t ON ct.tenant_id = t.id
   }
 });
 
-const path = require('path');
+// const path = require('path');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../test-app/dist')));
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '../test-app/dist')));
 
-// Catch-all handler: for any request that doesn't match an API route, send back React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../test-app/dist', 'index.html'));
-});
+// // Catch-all handler: for any request that doesn't match an API route, send back React's index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../test-app/dist', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
