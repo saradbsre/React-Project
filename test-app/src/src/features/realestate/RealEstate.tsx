6973c197e3015fe36c2041e2f429a571e9647099
saradbsre <sarad@binshabibgroup.ae>
2025-06-14 14:03:32 +0400

    Add files via upload
    Updated ui and added type checklist and images and video upload

---
+++ test-app/src/src/features/realestate/RealEstate.tsx
@@ -0,0 +1,11 @@
+// import AdminHeader from '../../components/adminheader/AdminHeader';
+export default function Dashboard() {
+  return (
+    <>
+      {/* <AdminHeader /> */}
+      <div className="">
+        Real Estate Works
+      </div>
+    </>
+  );
+}
